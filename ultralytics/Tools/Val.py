4377d6bfa6b7ab57168c2db3ebf5b130ea7509ae
UltralyticsAssistant <web@ultralytics.com>
2025-11-24 04:39:00 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/Tools/Val.py
+++ ultralytics/Tools/Val.py
@@ -1,10 +1,14 @@
-import matplotlib.pyplot as plt
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 from ultralytics import YOLO
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    model = YOLO(
+        "D://Work//PYworks//ultralytics-main//ultralytics//Tools//runs//detect//GAMTest1_mydataset_mypt_500//weights//best.pt"
+    )  # GAM
 
-    model = YOLO("D://Work//PYworks//ultralytics-main//ultralytics//Tools//runs//detect//GAMTest1_mydataset_mypt_500//weights//best.pt")  #GAM
-
-    metrics = model.val(data="../cfg/datasets/All_my_school_dataset.yaml", split="val", plots=True, project='./runs/Val',name='GAMVal')
+    metrics = model.val(
+        data="../cfg/datasets/All_my_school_dataset.yaml", split="val", plots=True, project="./runs/Val", name="GAMVal"
+    )
 
     print(metrics.results_dict)
